ea1c7facf263889e372d008523bb1bebac1cc19f
AdQuinti <quintidani2@gmail.com>
2025-06-19 00:07:17 +0200

    novedades

--- AutoGit.sh
+++ AutoGit.sh
@@ -1,7 +1,26 @@
 # Actualización forzada a repositorio git
 # https://github.com/AdQuinti/cvitae_adquinti.git
-echo "\n=== Subiendo cambios a GitHub - Forzado ==="
+#echo "\n=== Subiendo cambios a GitHub - Forzado ==="
+#git add .
+#git commit -m "Actualización automática $(date)"
+#git push -u -f origin main
+#echo "\n¡Proceso completado!"
+
+#!/bin/bash
+echo "⚙️  Añadiendo todos los archivos, incluyendo ignorados forzadamente..."
+
+# Fuerza agregar avatar.jpg (aunque esté en .gitignore)
+git add -f assets/external/avatar.jpg
+
+# Añade el resto normalmente (no forzará ignorados, solo avatar.jpg si quieres asegurarlo)
 git add .
-git commit -m "Actualización automática $(date)"
-git push -u -f origin main
-echo "\n¡Proceso completado!"
+
+echo "📦 Escriba su mensaje de commit:"
+read mensaje
+
+git commit -m "$mensaje"
+
+echo "⬆️ Subiendo a GitHub..."
+git push origin main
+
+echo "✅ ¡Subida completada!"
--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 source .venv/Scripts/activate
 pip install --upgrade pip
+echo "Creando archivo requerimientos..."
 pip install -r requirements.txt
+echo "Construyendo el proyecto Reflex..."
 reflex init
 reflex export --frontend-only
 rm -rf public
